fe7898309bb5e07c76d2cb7a06f19ff59eea6a07
alberto <alberto.perez25@alu.uclm.es>
2026-03-23 20:54:35 +0100

    #47 #54 Refactorizado nodo evaluador RAGAS para compatibilidad con el nuevo código

--- workspace/src/ruta_hospital/ruta_hospital/evaluation/system_evaluator_node.py
+++ workspace/src/ruta_hospital/ruta_hospital/evaluation/system_evaluator_node.py
@@ -8,10 +8,23 @@ from ruta_hospital.reporting.llm_reporter_node import LLMReporterNode
 from ruta_hospital.evaluation.ragas_evaluator import RagasEvaluator
 from ruta_hospital.evaluation.ragas_evaluator import OllamaParams
 
+from hospital_interfaces.action import GenerateReport
+
 DEFAULT_OLLAMA_URL = "http://localhost:11434"
 DEFAULT_EVALUATOR_LLM_MODEL = "llama3"
 DEFAULT_EVALUATOR_EMBED_MODEL = "nomic-embed-text"
 DEFAULT_QUESTIONS_PATH = "/home/alberto/tfg/Reconocimiento-y-sintesis-visual-Tiago/workspace/config/quest.json"
+DEFAULT_EVAL_FOLDER = "/home/alberto/tfg/Reconocimiento-y-sintesis-visual-Tiago/workspace/test_dataset/"
+
+class MockGoalHandle:
+    ''' Falso Goal Handle para reutilizar el código de LLMReporterNode
+        sin necesidad de levantar un Action Server real en la evaluación. '''
+    def __init__(self):
+        self.is_cancel_requested = False
+
+    def publish_feedback(self, msg):
+        # Ignoramos el feedback durante la evaluación en segundo plano
+        pass
 
 class SystemEvaluatorNode(Node):
     def __init__(self):
@@ -24,11 +37,13 @@ class SystemEvaluatorNode(Node):
         self.declare_parameter('evaluator_llm_model', DEFAULT_EVALUATOR_LLM_MODEL)
         self.declare_parameter('evaluator_embed_model', DEFAULT_EVALUATOR_EMBED_MODEL)
         self.declare_parameter('questions_path', DEFAULT_QUESTIONS_PATH)
+        self.declare_parameter('eval_folder_path', DEFAULT_EVAL_FOLDER)
 
         ollama_url = self.get_parameter('ollama_url').get_parameter_value().string_value
         llm_model = self.get_parameter('evaluator_llm_model').get_parameter_value().string_value
         embed_model = self.get_parameter('evaluator_embed_model').get_parameter_value().string_value
         quest_path = self.get_parameter('questions_path').get_parameter_value().string_value
+        self.eval_folder_path = self.get_parameter('eval_folder_path').get_parameter_value().string_value
 
         # Evaluador de Ragas
         ollama_params = OllamaParams(ollama_url = ollama_url, evaluator_llm_model = llm_model, evaluator_embed_model = embed_model)
@@ -46,15 +61,22 @@ class SystemEvaluatorNode(Node):
 
     async def evaluate_callback(self, request, response):
         self.get_logger().info("Iniciada Evaluación Ragas")
+
+        mock_result = GenerateReport.Result() 
+        mock_goal_handle = MockGoalHandle()
         
-        zone_groups = self.reporter_logic.validate_data(response)
+        zone_groups = self.reporter_logic.validate_data(self.eval_folder_path, mock_result)
         if not zone_groups:
+            response.success = False
+            response.message = mock_result.final_report # Captura el error
             return response
             
         self.get_logger().info("Extrayendo contexto de perceptores (YOLO/VLM)...")
-        global_context_json = await self.reporter_logic.process_each_image(zone_groups, response)
+        global_context_json = await self.reporter_logic.process_each_image(zone_groups, mock_goal_handle, mock_result)
         
-        if not global_context_json or self.reporter_logic.abort_processing:
+        if not global_context_json:
+            response.success = False
+            response.message = "Fallo en el procesamiento de imágenes por YOLO/VLM."
             return response
             
         self.get_logger().info("Generando respuestas...")
